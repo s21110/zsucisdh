aca37fa27fb390522a6218f8d44c92380b1a55fc
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-13 14:07:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,55 @@
 let 電子骰 = 0
 input.onGesture(Gesture.Shake, function () {
     電子骰 = randint(1, 6)
-    basic.showNumber(電子骰)
+    if (電子骰 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (電子骰 == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (電子骰 == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (電子骰 == 4) {
+        basic.showLeds(`
+            # . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . .
+            `)
+    } else if (電子骰 == 5) {
+        basic.showLeds(`
+            # . # . .
+            . . . . .
+            . # . . .
+            . . . . .
+            # . # . .
+            `)
+    } else if (電子骰 == 6) {
+        basic.showLeds(`
+            # . # . .
+            . . . . .
+            # . # . .
+            . . . . .
+            # . # . .
+            `)
+    }
 })
 basic.forever(function () {
 	
